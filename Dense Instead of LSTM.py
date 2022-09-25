23009b95cfddb6924a97c730427f1f94f8a4b279
Mohammad Mehdi Keramati Feyz Abadi <42337253+mohammadmehdikeramati@users.noreply.github.com>
2022-09-25 17:42:16 +0330

    Add files via upload

---
+++ Dense Instead of LSTM.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 21 17:14:48 2022
+
+@author: Mehdi Keramati
+"""
+
+
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+import keras
+import pandas as pd
+from keras import layers
+import matplotlib.pyplot as plt
+
+from tensorflow.keras import optimizers
+
+data = pd.read_csv("jena_climate_2009_2016.csv")
+
+raw_data = data.iloc[:, 1:]
+temperature = data.iloc[:, 2]
+
+
+num_train_samples = int(0.5 * len(raw_data))
+num_val_samples = int(0.25 * len(raw_data))
+num_test_samples = len(raw_data) - num_train_samples - num_val_samples
+
+print("num_train_samples:", num_train_samples)
+print("num_val_samples:", num_val_samples)
+print("num_test_samples:", num_test_samples)
+
+mean = raw_data[:num_train_samples].mean(axis=0)
+raw_data -= mean
+std = raw_data[:num_train_samples].std(axis=0)
+raw_data /= std
+
+
+sampling_rate = 6
+sequence_length = 120
+delay = sampling_rate * (sequence_length + 24 - 1)
+batch_size = 256
+
+train_dataset = keras.utils.timeseries_dataset_from_array(
+    raw_data[:-delay],
+    targets=temperature[delay:],
+    sampling_rate=sampling_rate,
+    sequence_length=sequence_length,
+    shuffle=True,
+    batch_size=batch_size,
+    start_index=0,
+    end_index=num_train_samples)
+
+val_dataset = keras.utils.timeseries_dataset_from_array(
+    raw_data[:-delay],
+    targets=temperature[delay:],
+    sampling_rate=sampling_rate,
+    sequence_length=sequence_length,
+    shuffle=True,
+    batch_size=batch_size,
+    start_index=num_train_samples,
+    end_index=num_train_samples + num_val_samples)
+
+test_dataset = keras.utils.timeseries_dataset_from_array(
+    raw_data[:-delay],
+    targets=temperature[delay:],
+    sampling_rate=sampling_rate,
+    sequence_length=sequence_length,
+    shuffle=True,
+    batch_size=batch_size,
+    start_index=num_train_samples + num_val_samples)
+
+for samples, targets in train_dataset:
+    print("samples shape:", samples.shape)
+    print("targets shape:", targets.shape)
+    break   
+
+
+######################### Model ########################################
+
+inputs = keras.Input(shape=(sequence_length, raw_data.shape[-1]))
+x = layers.Dense(32)(inputs)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
+
+
+model.compile(optimizer="rmsprop", loss="mse", metrics=["mae"])
+history = model.fit(train_dataset,
+                    epochs=10,
+                    validation_data=val_dataset)
+
